6795a1709b79f047cd136fe1507f94fcb83fc284
LegeBeker <volkanwelp@gmail.com>
2023-03-29 23:25:09 +0200

    ✨ Add check login function

--- src/db/AccountDB.java
+++ src/db/AccountDB.java
@@ -1,5 +1,16 @@
 package db;
 
 public class AccountDB {
-    
+    public static Boolean checkLogin(String username, String password) {
+        Database db = Database.getInstance();
+
+        String sql = "SELECT * FROM account WHERE username = ? AND password = ?";
+        String[] params = { username, password };
+
+        if (db.exec(sql, params).isEmpty()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
