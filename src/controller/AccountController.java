79fcac87b92546df99e9853049b8f565b93155f2
LegeBeker <volkanwelp@gmail.com>
2023-03-29 23:24:43 +0200

    ✨ Update loginaccount function

--- src/controller/AccountController.java
+++ src/controller/AccountController.java
@@ -1,11 +1,20 @@
 package controller;
 
+import model.Account;
+
 public class AccountController {
 	public Account curAccount;
-	
-	public Boolean login(String username, String password) {
-		Account tempAccount = new Account();
-		tempAccount.loginAccount(username, password);
-		this.curAccount = tempAccount;
+
+	public Boolean loginAccount(String username, String password) {
+		Account curAccount = new Account();
+
+		curAccount.setUsername(username);
+		curAccount.setPassword(password);
+
+		if (!curAccount.login()) {
+			return false;
+		}
+		this.curAccount = curAccount;
+		return true;
 	}
 }
