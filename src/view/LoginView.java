2a382b8f679494b3501def85e52e445878331b2f
LegeBeker <volkanwelp@gmail.com>
2023-03-29 19:28:07 +0200

    ✨ Add login view

---
+++ src/view/LoginView.java
@@ -0,0 +1,40 @@
+package view;
+
+import controller.ViewController;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class LoginView extends VBox {
+	
+	private ViewController view;
+	
+	private TextField fieldUsername;
+	private TextField fieldPassword;
+	
+	private Button buttonSubmit;
+	
+	public LoginView(ViewController view) {
+		this.view = view;
+		
+		this.setAlignment(Pos.CENTER);
+		
+		this.fieldUsername = new TextField();
+
+		this.fieldPassword = new TextField();
+
+		this.buttonSubmit = new Button("Inloggen");
+		this.buttonSubmit.setOnAction(e -> this.submit());
+
+		this.getChildren().addAll(this.fieldUsername, this.fieldPassword, this.buttonSubmit);
+	}
+	
+	public void submit() {
+		if (view.accountController.login(this.fieldUsername.getText(), this.fieldPassword.getText())) {
+			view.OpenMenuView();
+		} else {
+			// throw exception
+		}
+	}
+}
