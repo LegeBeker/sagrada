f45c4bb534681c500468f60684b76b16636896b9
Lars van Herwijnen <77294984+larsvanherwijnen@users.noreply.github.com>
2023-03-29 20:21:56 +0200

    ✨ Created contructor database class

--- src/db/Database.java
+++ src/db/Database.java
@@ -4,4 +4,17 @@ import pattern.Singleton;
 
 public final class Database extends Singleton {
 
+    private String databaseName;
+    private String userName;
+    private String password;
+    private String host;
+    private int port = 3306;
+
+
+    public Database(String name, String username, String password, String host){
+        this.databaseName = name;
+        this.userName = username;
+        this.password = password;
+        this.host = host;
+    }
 }
