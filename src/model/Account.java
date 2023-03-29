dfa79e4757495665ee0e2e6fb38868a9741b12a1
LegeBeker <volkanwelp@gmail.com>
2023-03-29 19:27:14 +0200

    ✨ Add account model

---
+++ src/model/Account.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Account {
+	private String username;
+	private String password;
+	
+	public Account loginAccount(String username, String password) {
+		if () {
+	}
+}
