a8153f55e46af404f0cefb4793bad24ba2729faa
LegeBeker <volkanwelp@gmail.com>
2023-03-29 19:26:32 +0200

    ✨ Add viewcontroller

---
+++ src/controller/ViewController.java
@@ -0,0 +1,47 @@
+package controller;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import view.LoginView;
+import view.RegisterView;
+import view.MenuView;
+import view.GamesView;
+import view.StatsView;
+
+public class ViewController extends Scene {
+	
+	public AccountController accountController;
+	
+	public ViewController() {
+		super(new Pane());
+		
+		this.accountController = new AccountController();
+		
+		this.OpenLoginView();
+	}
+	
+	public void OpenLoginView() {
+		LoginView loginView = new LoginView(this);
+		setRoot(loginView);
+	}
+	
+	public void OpenRegisterView() {
+		RegisterView registerView = new RegisterView(this);
+		setRoot(registerView);
+	}
+	
+	public void OpenMenuView() {
+		MenuView menuView = new MenuView(this);
+		setRoot(menuView);
+	}
+	
+	public void OpenGamesView() {
+		GamesView gamesView = new GamesView(this);
+		setRoot(gamesView);
+	}
+	
+	public void OpenStatsView() {
+		StatsView statsView = new StatsView(this);
+		setRoot(statsView);
+	}
+}
