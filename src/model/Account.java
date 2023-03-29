115111a0e7abca11f2833030cb89cdf1a4d6a7ad
LegeBeker <volkanwelp@gmail.com>
2023-03-29 23:26:07 +0200

    ✨ Add account model logic

--- src/model/Account.java
+++ src/model/Account.java
@@ -1,10 +1,20 @@
 package model;
 
+import db.AccountDB;
+
 public class Account {
 	private String username;
 	private String password;
-	
-	public Account loginAccount(String username, String password) {
-		if () {
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Boolean login() {
+		return AccountDB.checkLogin(username, password);
 	}
 }
