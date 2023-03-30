5cbdca32b90657f9c887a69ce5e4e36629676e03
LegeBeker <volkanwelp@gmail.com>
2023-03-30 20:55:17 +0200

    💄 Improve styling

--- src/view/MenuView.java
+++ src/view/MenuView.java
@@ -5,27 +5,58 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 public class MenuView extends VBox {
 
+	private ViewController view;
+
 	private Text textGreet;
+
 	private Button buttonGames;
 	private Button buttonStats;
+	private Button buttonLogout;
+
+	private int buttonHeight = 25;
+	private int buttonWidth = 200;
+
+	private int padding = 200;
+	private int spacing = 15;
 
 	public MenuView(ViewController view) {
+		this.view = view;
+		this.setBackground(view.getBackground());
+
 		this.setAlignment(Pos.CENTER);
 
 		this.textGreet = new Text("Welkom " + view.getAccountController().getAccount().getUsername() + "!");
+		this.textGreet.setStyle("-fx-font-size: 20px;");
+		this.textGreet.setWrappingWidth(this.buttonWidth);
+		this.textGreet.setTextAlignment(TextAlignment.CENTER);
+		this.textGreet.setFill(Color.WHITE);
 
 		this.buttonGames = new Button("Spellen");
-		this.buttonGames.setOnAction(e -> view.OpenGamesView());
+		this.buttonGames.setPrefSize(this.buttonWidth, this.buttonHeight);
+		this.buttonGames.setOnAction(e -> view.openGamesView());
 
 		this.buttonStats = new Button("Statistieken");
-		this.buttonGames.setOnAction(e -> view.OpenStatsView());
+		this.buttonStats.setPrefSize(this.buttonWidth, this.buttonHeight);
+		this.buttonStats.setOnAction(e -> view.openStatsView());
 
-		this.setPadding(new Insets(10));
+		this.buttonLogout = new Button("Uitloggen");
+		this.buttonLogout.setPrefSize(this.buttonWidth, this.buttonHeight);
+		this.buttonLogout.setOnAction(e -> this.logout());
 
-		this.getChildren().addAll(this.textGreet, this.buttonGames, this.buttonStats);
+		this.setSpacing(this.spacing);
+		this.setPadding(new Insets(0, this.padding, 0, this.padding));
+
+		this.getChildren().addAll(view.getLogo(), this.textGreet, this.buttonGames, this.buttonStats, this.buttonLogout);
+	}
+
+	private void logout() {
+		view.getAccountController().logoutAccount();
+		view.openLoginView();
 	}
 }
--- src/view/RegisterView.java
+++ src/view/RegisterView.java
@@ -1,9 +1,11 @@
 package view;
 
 import controller.ViewController;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class RegisterView extends VBox {
@@ -13,25 +15,47 @@ public class RegisterView extends VBox {
     private TextField fieldUsername;
 	private TextField fieldPassword;
 
+	private HBox boxButtons;
+
 	private Button buttonSubmit;
-	private Button buttonRegister; 
+	private Button buttonLoginPage;
+
+	private int buttonHeight = 25;
+	private int buttonWidth = 200;
+
+	private int padding = 200;
+	private int spacing = 15;
 
     public RegisterView(ViewController view) {
         this.view = view;
+		this.setBackground(view.getBackground());
 
 		this.setAlignment(Pos.CENTER);
 
 		this.fieldUsername = new TextField();
+		this.fieldUsername.setPromptText("Gebruikersnaam");
 
 		this.fieldPassword = new TextField();
+		this.fieldPassword.setPromptText("Wachtwoord");
 
 		this.buttonSubmit = new Button("Registeren");
+		this.buttonSubmit.setPrefSize(this.buttonWidth, this.buttonHeight);
 		this.buttonSubmit.setOnAction(e -> this.submit());
 
-		this.buttonRegister = new Button("Login pagina");
-		this.buttonRegister.setOnAction(e -> this.login());
+		this.buttonLoginPage = new Button("Login pagina");
+		this.buttonLoginPage.setPrefSize(this.buttonWidth, this.buttonHeight);
+		this.buttonLoginPage.setOnAction(e -> this.openLoginView());
 
-		this.getChildren().addAll(this.fieldUsername, this.fieldPassword, this.buttonSubmit, this.buttonRegister);
+		this.boxButtons = new HBox();
+		this.boxButtons.getChildren().addAll(this.buttonSubmit, this.buttonLoginPage);
+
+		this.boxButtons.setAlignment(Pos.CENTER);
+		this.boxButtons.setSpacing(this.spacing);
+
+		this.setSpacing(this.spacing);
+		this.setPadding(new Insets(0, this.padding, 0, this.padding));
+
+		this.getChildren().addAll(view.getLogo(), this.fieldUsername, this.fieldPassword, this.boxButtons);
     }
 
 
@@ -40,10 +64,10 @@ public class RegisterView extends VBox {
             throw new RuntimeException("Register failed");
         }
 
-        view.OpenMenuView();
+        view.openMenuView();
     }
 
-    public void login(){
-        view.OpenLoginView();
+    public void openLoginView(){
+        view.openLoginView();
     }
 }
