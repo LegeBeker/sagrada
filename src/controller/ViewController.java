148dc11697f7e9c218c8d7859db02a7fe7b6b965
LegeBeker <volkanwelp@gmail.com>
2023-03-30 20:55:37 +0200

    ✨ Add logo background and font

--- src/controller/ViewController.java
+++ src/controller/ViewController.java
@@ -1,7 +1,13 @@
 package controller;
 
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import view.LoginView;
 import view.RegisterView;
 import view.MenuView;
@@ -12,35 +18,53 @@ public class ViewController extends Scene {
 
 	private AccountController accountController;
 
+	private ImageView logo = new ImageView(new Image("file:resources/img/logo.png"));
+	private Background background = new Background(new BackgroundFill(Color.web("#4483c2"), null, null));
+	private Font font = new Font("Arial", 20);
+
 	public ViewController() {
 		super(new Pane());
 
 		this.accountController = new AccountController();
 
-		this.OpenLoginView();
+		this.openLoginView();
 	}
 
-	public void OpenLoginView() {
+	public ImageView getLogo() {
+		this.logo.setFitWidth(300);
+		this.logo.setPreserveRatio(true);
+		return this.logo;
+	}
+
+	public Background getBackground() {
+		return this.background;
+	}
+
+	public Font getFont() {
+		return this.font;
+	}
+
+	public void openLoginView() {
 		LoginView loginView = new LoginView(this);
 		setRoot(loginView);
 	}
 
-	public void OpenRegisterView() {
+	public void openRegisterView() {
 		RegisterView registerView = new RegisterView(this);
 		setRoot(registerView);
 	}
 
-	public void OpenMenuView() {
+	public void openMenuView() {
 		MenuView menuView = new MenuView(this);
 		setRoot(menuView);
 	}
 
-	public void OpenGamesView() {
+	public void openGamesView() {
 		GamesView gamesView = new GamesView(this);
 		setRoot(gamesView);
 	}
 
-	public void OpenStatsView() {
+	public void openStatsView() {
 		StatsView statsView = new StatsView(this);
 		setRoot(statsView);
 	}
