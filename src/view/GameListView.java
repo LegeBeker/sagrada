0bd0e1c6b5d72a6aacc6136f487fd388d437f2dd
LegeBeker <volkanwelp@gmail.com>
2023-04-02 14:46:42 +0200

    💄 Improve games view

--- src/view/GameListView.java
+++ src/view/GameListView.java
@@ -1,14 +1,18 @@
 package view;
 
 import controller.ViewController;
+import javafx.geometry.Pos;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import model.Game;
 
-public class GameListView extends FlowPane {
+public class GameListView extends HBox {
 
     private ViewController view;
     private Game game;
@@ -21,24 +25,52 @@ public class GameListView extends FlowPane {
 
     private final int height = 50;
 
+    private final double widthFraction = 1.0 / 6.0;
+
     public GameListView(final ViewController view, final Game game) {
         this.view = view;
         this.game = game;
 
         this.setBackground(new Background(new BackgroundFill(Color.web("#73b4f5"), null, null)));
-        this.setPrefHeight(height);
+        this.setPrefHeight(this.height);
+
+        this.setMinWidth(Region.USE_PREF_SIZE);
 
         this.setStyle("-fx-border-color: white; -fx-border-width: 1px;");
 
-        this.textId = new Text("Id: " + this.game.getId());
+        this.textId = new Text(Integer.toString(this.game.getId()));
+        this.textId.setFill(Color.WHITE);
+        StackPane textIdBox = new StackPane(this.textId);
+        textIdBox.prefWidthProperty().bind(this.widthProperty().multiply(widthFraction));
+        textIdBox.setStyle("-fx-border-color: white; -fx-border-width: 1px;");
 
-        this.textTurnPlayer = new Text("Beurtspeler: " + this.game.getTurnPlayer().getUsername());
+        this.textTurnPlayer = new Text(this.game.getTurnPlayer().getUsername());
+        this.textTurnPlayer.setFill(Color.WHITE);
+        StackPane textTurnPlayerBox = new StackPane(this.textTurnPlayer);
+        textTurnPlayerBox.prefWidthProperty().bind(this.widthProperty().multiply(2 * widthFraction));
+        textTurnPlayerBox.setStyle("-fx-border-color: white; -fx-border-width: 1px;");
 
-        this.textRoundNumber = new Text("Ronde: " + this.game.getCurrentRound());
+        this.textRoundNumber = new Text(Integer.toString(this.game.getCurrentRound()));
+        this.textRoundNumber.setFill(Color.WHITE);
+        StackPane textRoundNumberBox = new StackPane(this.textRoundNumber);
+        textRoundNumberBox.prefWidthProperty().bind(this.widthProperty().multiply(widthFraction));
+        textRoundNumberBox.setStyle("-fx-border-color: white; -fx-border-width: 1px;");
 
-        this.textDate = new Text("Datum: " + this.game.getCreationDate());
+        this.textDate = new Text(this.game.getCreationDate());
+        this.textDate.setFill(Color.WHITE);
+        StackPane textDateBox = new StackPane(this.textDate);
+        textDateBox.prefWidthProperty().bind(this.widthProperty().multiply(2 * widthFraction));
+        textDateBox.setStyle("-fx-border-color: white; -fx-border-width: 1px;");
 
-        this.getChildren().addAll(this.textId, this.textTurnPlayer, this.textRoundNumber, this.textDate);
+        this.getChildren().addAll(textIdBox, textTurnPlayerBox, textRoundNumberBox, textDateBox);
+        this.setAlignment(Pos.CENTER);
+
+        HBox.setHgrow(textIdBox, Priority.ALWAYS);
+        HBox.setHgrow(textTurnPlayerBox, Priority.ALWAYS);
+        HBox.setHgrow(textRoundNumberBox, Priority.ALWAYS);
+        HBox.setHgrow(textDateBox, Priority.ALWAYS);
+
+        HBox.setHgrow(this, Priority.ALWAYS);
 
         this.setOnMouseClicked(e -> this.openGameView());
     }
@@ -49,6 +81,6 @@ public class GameListView extends FlowPane {
     }
 
     public void openGameView() {
-            this.view.openGameView(this.game);
+        this.view.openGameView(this.game);
     }
 }
--- src/view/GamesView.java
+++ src/view/GamesView.java
@@ -7,7 +7,11 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
@@ -17,14 +21,18 @@ public class GamesView extends VBox {
 
     private ViewController view;
 
-    private Text textTitle;
+    private StackPane textTitle;
+
+    private HBox boxInfo;
+
+    private final int boxInfoHeight = 10;
 
     private ScrollPane scrollBox;
 
     private VBox boxGames;
 
     private final int scrollBoxHeight = 300;
-    private final int scrollBoxWidth = 600;
+    private final int scrollBoxWidth = 400;
 
     private ArrayList<Game> games;
 
@@ -39,22 +47,66 @@ public class GamesView extends VBox {
     private final int padding = 200;
     private final int spacing = 15;
 
+    private final double widthFraction = 1.0 / 6.0;
+
     public GamesView(final ViewController view) {
         this.view = view;
         this.setBackground(view.getBackground());
 
         this.setAlignment(Pos.CENTER);
 
-        this.textTitle = new Text("Alle Spellen");
-        this.textTitle.setStyle("-fx-font-size: 40px");
-        this.textTitle.setStroke(Color.web("#000000"));
-        this.textTitle.setFill(Color.web("#ffffff"));
+        Text text = new Text("Alle Spellen");
+        text.setStyle("-fx-font-size: 40px");
+        text.setStroke(Color.web("#000000"));
+        text.setFill(Color.web("#ffffff"));
+
+        this.textTitle = new StackPane(text);
+        this.textTitle.setPadding(new Insets(0, 0, this.spacing, 0));
+
+        this.boxInfo = new HBox();
+        this.boxInfo.setAlignment(Pos.CENTER);
+        this.boxInfo.setBackground(new Background(new BackgroundFill(Color.web("#4591de"), null, null)));
+        this.boxInfo.setPrefSize(this.scrollBoxWidth, this.boxInfoHeight);
+
+        Text idText = new Text("Id");
+        idText.setFill(Color.WHITE);
+        StackPane idTextBox = new StackPane(idText);
+        idTextBox.prefWidthProperty().bind(this.boxInfo.widthProperty().multiply(widthFraction));
+        idTextBox.setStyle("-fx-border-color: white; -fx-border-width: 1px;");
+
+        Text playerText = new Text("Beurtspeler");
+        playerText.setFill(Color.WHITE);
+        StackPane playerTextBox = new StackPane(playerText);
+        playerTextBox.prefWidthProperty().bind(this.boxInfo.widthProperty().multiply(2 * widthFraction));
+        playerTextBox.setStyle("-fx-border-color: white; -fx-border-width: 1px;");
+
+        Text roundText = new Text("Ronde");
+        roundText.setFill(Color.WHITE);
+        StackPane roundTextBox = new StackPane(roundText);
+        roundTextBox.prefWidthProperty().bind(this.boxInfo.widthProperty().multiply(widthFraction));
+        roundTextBox.setStyle("-fx-border-color: white; -fx-border-width: 1px;");
+
+        Text dateText = new Text("Datum");
+        dateText.setFill(Color.WHITE);
+        StackPane dateTextBox = new StackPane(dateText);
+        dateTextBox.prefWidthProperty().bind(this.boxInfo.widthProperty().multiply(2 * widthFraction));
+        dateTextBox.setStyle("-fx-border-color: white; -fx-border-width: 1px;");
+
+        this.boxInfo.getChildren().addAll(idTextBox, playerTextBox, roundTextBox, dateTextBox);
+        this.boxInfo.setPadding(new Insets(0, 0, 0, 0));
+
+        HBox.setHgrow(idTextBox, Priority.ALWAYS);
+        HBox.setHgrow(playerTextBox, Priority.ALWAYS);
+        HBox.setHgrow(roundTextBox, Priority.ALWAYS);
+        HBox.setHgrow(dateTextBox, Priority.ALWAYS);
 
         this.scrollBox = new ScrollPane();
 
         this.games = view.getGameController().getGames();
 
         this.boxGames = new VBox();
+        this.boxGames.setAlignment(Pos.CENTER);
+        this.boxGames.prefWidthProperty().bind(this.scrollBox.widthProperty());
 
         for (Game game : this.games) {
             this.boxGames.getChildren().add(new GameListView(this.view, game));
@@ -63,6 +115,13 @@ public class GamesView extends VBox {
         this.scrollBox.setContent(this.boxGames);
 
         this.scrollBox.setPrefSize(scrollBoxWidth, scrollBoxHeight);
+        this.scrollBox.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        this.scrollBox.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+
+        this.scrollBox.setPadding(new Insets(0, 0, 0, 0));
+
+        // fill full width of scrollbox with boxGames
+        this.boxGames.prefWidthProperty().bind(this.scrollBox.widthProperty());
 
         this.buttonBack = new Button("Terug");
         this.buttonBack.setPrefSize(this.buttonWidth, this.buttonHeight);
@@ -77,11 +136,11 @@ public class GamesView extends VBox {
 
         this.boxButtons.setAlignment(Pos.CENTER);
         this.boxButtons.setSpacing(this.spacing);
+        this.boxButtons.setPadding(new Insets(this.spacing, 0, this.spacing, 0));
 
-        this.setSpacing(this.spacing);
         this.setPadding(new Insets(0, this.padding, 0, this.padding));
 
-        this.getChildren().addAll(this.textTitle, this.scrollBox, this.boxButtons);
+        this.getChildren().addAll(this.textTitle, this.boxInfo, this.scrollBox, this.boxButtons);
     }
 
     private void back() {
