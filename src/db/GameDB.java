392fb1fdcd83122d3f34f8e34975c7715d6ffc32
LegeBeker <volkanwelp@gmail.com>
2023-04-02 12:19:30 +0200

    ✨ Add game get in db class

--- src/db/GameDB.java
+++ src/db/GameDB.java
@@ -7,6 +7,15 @@ public final class GameDB {
     private GameDB() {
     }
 
+    public static Map<String, String> get(final int idGame) {
+        Database db = Database.getInstance();
+
+        String sql = "SELECT * FROM game WHERE idgame = ? LIMIT 1";
+        String[] params = {Integer.toString(idGame)};
+
+        return db.exec(sql, params).get(0);
+    }
+
     public static List<Map<String, String>> getAll() {
         Database db = Database.getInstance();
 
