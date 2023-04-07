d8a1b6340572a10ed8f74a40222e0ffe4429a642
Lars van Herwijnen <77294984+larsvanherwijnen@users.noreply.github.com>
2023-04-07 16:34:43 +0200

    ✨ Add GameScoreView

---
+++ src/main/java/view/GameViews/GameScoreView.java
@@ -0,0 +1,14 @@
+package main.java.view.GameViews;
+
+import javafx.scene.layout.HBox;
+import main.java.controller.ViewController;
+import main.java.model.Game;
+
+public class GameScoreView extends HBox {
+
+    public GameScoreView(ViewController view, Game game) {
+        
+
+    }
+    
+}
