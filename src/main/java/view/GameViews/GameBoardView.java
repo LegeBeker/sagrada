47dc87e912671aec20b3814055f44bfd684854ab
Lars van Herwijnen <77294984+larsvanherwijnen@users.noreply.github.com>
2023-04-07 16:32:58 +0200

    ✨ Add gameboardview

---
+++ src/main/java/view/GameViews/GameBoardView.java
@@ -0,0 +1,12 @@
+package main.java.view.GameViews;
+
+import javafx.scene.layout.GridPane;
+import main.java.controller.ViewController;
+import main.java.model.Game;
+
+public class GameBoardView extends GridPane {
+
+    public GameBoardView(ViewController view, Game game) {
+    }
+    
+}
