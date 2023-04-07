e2b9ce479b176959aca846f736e14857ca4b040f
Lars van Herwijnen <77294984+larsvanherwijnen@users.noreply.github.com>
2023-04-07 16:34:09 +0200

    ✨ Add GameInfoView

---
+++ src/main/java/view/GameViews/GameInfoView.java
@@ -0,0 +1,16 @@
+package main.java.view.GameViews;
+
+import javafx.scene.layout.HBox;
+import main.java.controller.ViewController;
+import main.java.model.Game;
+
+public class GameInfoView extends HBox {
+
+    public GameInfoView() {
+        
+    }
+
+    public GameInfoView(ViewController view, Game game) {
+    }
+
+}
