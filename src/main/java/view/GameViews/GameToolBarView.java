aaecab826a39b0ea14ee3895f0d7da38f04ef955
Lars van Herwijnen <77294984+larsvanherwijnen@users.noreply.github.com>
2023-04-07 19:41:15 +0200

    🚚 Rename GameInfoView to GameToolBarView

--- src/main/java/view/GameViews/GameToolBarView.java
+++ src/main/java/view/GameViews/GameToolBarView.java
@@ -8,12 +8,12 @@ import javafx.scene.text.Text;
 import main.java.controller.ViewController;
 import main.java.model.Game;
 
-public class GameInfoView extends HBox {
+public class GameToolBarView extends HBox {
     private final Background background = new Background(new BackgroundFill(Color.web("#777564"), null, null));
 
     private final Text text = new Text("Dobbelstenen en gereedschapkaarten");
 
-    public GameInfoView(ViewController view, Game game) {
+    public GameToolBarView(ViewController view, Game game) {
         this.setBackground(background);
         this.getChildren().add(text);
 
