adce39bd6efb501a4d4447f166a3ba05368c8f25
Lars van Herwijnen <77294984+larsvanherwijnen@users.noreply.github.com>
2023-04-07 16:35:18 +0200

    🚚 Moved GameView

---
+++ src/main/java/view/GameViews/GameView.java
@@ -0,0 +1,23 @@
+package main.java.view.GameViews;
+
+import javafx.scene.layout.BorderPane;
+import main.java.controller.ViewController;
+import main.java.model.Game;
+
+public class GameView extends BorderPane {
+
+    private ViewController view;
+    private Game game;
+
+    public GameView(final ViewController view, final Game game) {
+        this.view = view;
+        this.game = game;
+
+        this.setLeft(new GameScoreView(view, game));
+        this.setCenter(new GameBoardView(view, game));
+        this.setBottom(new GameInfoView(view, game));
+        this.setRight(new GameChatView(view, game));
+        
+        this.setBackground(view.getBackground());
+    }
+}
