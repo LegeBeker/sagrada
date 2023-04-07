f75e2029927aef5ce01921f47fa9c8cf59ce6c12
Lars van Herwijnen <77294984+larsvanherwijnen@users.noreply.github.com>
2023-04-07 16:33:27 +0200

    ✨ Add game chat view

---
+++ src/main/java/view/GameViews/GameChatView.java
@@ -0,0 +1,15 @@
+package main.java.view.GameViews;
+
+import javafx.scene.layout.HBox;
+import main.java.controller.ViewController;
+import main.java.model.Game;
+
+public class GameChatView extends HBox {
+
+    public GameChatView() {
+    }
+
+    public GameChatView(ViewController view, Game game) {
+    }
+
+}
