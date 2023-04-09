5c0fe5e2c70efdebea83635752c72f064c46dce5
Volkan Welp <49623578+LegeBeker@users.noreply.github.com>
2023-04-09 12:30:16 +0200

    🚚 Move functions so it is not static

--- src/main/java/model/PatternCardField.java
+++ src/main/java/model/PatternCardField.java
@@ -7,16 +7,35 @@ public class PatternCardField {
     private Color color;
     private int value;
 
-    public PatternCardField(final Color color, final int value) {
-        this.color = color;
-        this.value = value;
-    }
-
     public Color getColor() {
         return this.color;
     }
 
     public int getValue() {
         return this.value;
+    }
+
+    public void setColor(final String color) {
+        switch (color) {
+            case "red":
+                this.color = Color.web("#F44336");
+            case "blue":
+                this.color = Color.web("#2196F3");
+            case "green":
+                this.color = Color.web("#4CAF50");
+            case "yellow":
+                this.color = Color.web("#FFEB3B");
+            case "purple":
+                this.color = Color.web("#9C27B0");
+            default:
+                this.color = Color.web("#F8F8F8");
+        }
+    }
+
+    public void setValue(final String value) {
+        this.value = 0;
+        if (value != null) {
+            this.value = Integer.parseInt(value);
+        }
     }
 }
