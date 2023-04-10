efc65dfa6bea42a98602953f580fad0840995498
Mike de Groot <mike251105@gmail.com>
2023-04-10 21:33:34 +0200

    ✨ Addmessage function

--- src/main/java/view/GameChatView.java
+++ src/main/java/view/GameChatView.java
@@ -1,7 +1,13 @@
 package main.java.view;
 
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.FlowPane;
@@ -17,16 +23,15 @@ public class GameChatView extends VBox {
 
     private final Background background = new Background(new BackgroundFill(Color.web("#B00322"), null, null));
 
-    private VBox chatBox = new VBox();
-    private FlowPane chatMessage = new FlowPane();
+    private VBox chatMessageBox = new VBox();
     private HBox chatInput = new HBox();
-
+    private ViewController view;
     private final int buttonRatio = 3;
 
     public GameChatView(final ViewController view, final Game game) {
         this.setBackground(background);
 
-        System.out.println(this.getPrefHeight());
+        this.setAlignment(Pos.BOTTOM_CENTER);
 
         TextField textInput = new TextField();
         textInput.setPromptText("Typ je geweldige bericht hier");
@@ -41,7 +46,7 @@ public class GameChatView extends VBox {
                 if (event.getCode() == KeyCode.ENTER) {
                     addMessage(textInput.getText());
                     textInput.setText("");
-            }
+                }
             }
         });
 
@@ -62,4 +67,15 @@ public class GameChatView extends VBox {
         this.getChildren().add(1, chatInput);
     }
 
+    private void addMessage(final String message) {
+        FlowPane chatMessage;
+        chatMessage = new FlowPane();
+        chatMessage.setBackground(null);
+        chatMessage.getChildren().add(new Text("Speler 1: "));
+        chatMessage.getChildren().add(new Text(message));
+
+        System.out.println(this.getWidth());
+
+        chatMessageBox.getChildren().add(chatMessage);
+    }
 }
