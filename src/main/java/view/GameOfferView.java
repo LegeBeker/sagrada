8a3f52b5efa852e41fd5f07e2f374e96700d1623
Eren <128692641+Eren073@users.noreply.github.com>
2023-04-13 15:47:44 +0200

    added gameofferview

--- src/main/java/view/GameOfferView.java
+++ src/main/java/view/GameOfferView.java
@@ -2,9 +2,17 @@ package main.java.view;
 
 import javafx.scene.layout.FlowPane;
 import main.java.controller.ViewController;
+import main.java.model.Die;
 import main.java.model.Game;
 
 public class GameOfferView extends FlowPane {
-    public GameOfferView(final ViewController view, final Game game) {
-    }
+	public GameOfferView(final ViewController view, final Game game) {
+
+		for (Die die : game.getOffer()) {
+			DieView dieView = new DieView(die.getEyes(), die.getColor());
+
+			this.getChildren().add(dieView);
+		}
+
+	}
 }
