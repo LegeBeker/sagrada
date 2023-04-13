e99ee44edd612443212c6b93b6174001e6fe82dc
Eren <128692641+Eren073@users.noreply.github.com>
2023-04-13 15:45:34 +0200

    added function put offer

--- src/main/java/db/DieDB.java
+++ src/main/java/db/DieDB.java
@@ -5,19 +5,27 @@ import java.util.Map;
 
 public class DieDB {
 
-	public static List<Map<String, String>> getOffer(final int idGame, final int dieAmount) {
-        Database db = Database.getInstance();
+	public static boolean putOffer(final int idGame, final int dieAmount) {
+		Database db = Database.getInstance();
 
-        String sql = "INSERT INTO gamedie (idgame, diecolor, dienumber, eyes) "
-        		+ "SELECT ?, d.color, d.number, FLOOR(RAND()* 6) + 1 "
-        		+ "FROM die d "
-        		+ "LEFT JOIN gamedie g "
-        		+ "ON d.color = g.diecolor AND d.number = g.dienumber AND g.idgame = ? "
-        		+ "WHERE g.idgame IS NULL "
-        		+ "ORDER BY RAND() "
-        		+ "LIMIT ?;";
-        String[] params = {Integer.toString(idGame),Integer.toString(idGame), Integer.toString(dieAmount)};
+		String sql = "INSERT INTO gamedie (idgame, diecolor, dienumber, eyes) "
+				+ "SELECT ?, d.color, d.number, FLOOR(RAND()* 6) + 1 " + "FROM die d " + "LEFT JOIN gamedie g "
+				+ "ON d.color = g.diecolor AND d.number = g.dienumber AND g.idgame = ? " + "WHERE g.idgame IS NULL "
+				+ "ORDER BY RAND() " + "LIMIT ?;";
+		String[] params = { Integer.toString(idGame), Integer.toString(idGame), Integer.toString(dieAmount) };
+		
+			db.exec(sql, params);
 
-        return db.exec(sql, params);
-    }
+			return true;
+	}
+
+	public static List<Map<String, String>> getOffer(final int idGame) {
+		Database db = Database.getInstance();
+
+		String sql = "SELECT * FROM gamedie WHERE idgame = ? AND eyes IS NOT NULL AND roundtrack IS NULL AND roundID IS NULL";
+
+		String[] params = { Integer.toString(idGame) };
+
+		return db.exec(sql, params);
+	}
 }
