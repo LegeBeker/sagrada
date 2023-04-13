14c5521702c81eaf16d22ee65bc6cccd27840275
Eren <128692641+Eren073@users.noreply.github.com>
2023-04-13 15:47:17 +0200

    added get dice button

--- src/main/java/view/GameButtonsView.java
+++ src/main/java/view/GameButtonsView.java
@@ -8,35 +8,47 @@ import main.java.model.Game;
 
 public class GameButtonsView extends FlowPane {
 
-    private ViewController view;
+	private ViewController view;
 
-    private Button buttonBack;
-    private Button buttonEndTurn;
+	private Button buttonBack;
+	private Button buttonEndTurn;
+	private Button buttonGetDice;
 
-    private final int padding = 10;
+	private final int padding = 10;
 
-    public GameButtonsView(final ViewController view, final Game game) {
-        this.view = view;
+	public GameButtonsView(final ViewController view, final Game game) {
+		this.view = view;
 
-        this.buttonBack = new Button("Terug");
-        this.buttonEndTurn = new Button("Einde beurt");
+		this.buttonBack = new Button("Terug");
 
-        this.buttonEndTurn.setOnAction(e -> {
-            game.endTurn();
-        });
+		this.buttonEndTurn = new Button("Einde beurt");
 
-        this.buttonBack.setOnAction(e -> {
-            view.openGamesView();
-        });
+		this.buttonGetDice = new Button("Pak dobbelstenen");
 
-        this.getChildren().add(buttonBack);
+		this.buttonEndTurn.setOnAction(e -> game.endTurn());
 
-        if (game.getTurnPlayer().getUsername().equals(view.getAccountController().getAccount().getUsername())) {
-            this.getChildren().addAll(buttonEndTurn);
-        }
+		this.buttonBack.setOnAction(e -> view.openGamesView());
 
-        this.setPadding(new Insets(padding, padding, padding, padding));
-        this.setHgap(padding);
-        this.setVgap(padding);
-    }
+		this.buttonGetDice.setOnAction(e -> game.getNewOffer());
+
+		this.getChildren().addAll(this.buttonBack, this.buttonGetDice);
+
+		if (game.getTurnPlayer().getUsername().equals(view.getAccountController().getAccount().getUsername())) {
+			this.getChildren().addAll(buttonEndTurn);
+		}
+
+		this.setPadding(new Insets(padding, padding, padding, padding));
+		this.setHgap(padding);
+		this.setVgap(padding);
+	}
+	
+	public void getDice(final Game game) {
+		try {
+			game.getNewOffer();
+		}
+		catch(RuntimeException e){
+			this.view.displayError(e.getMessage());
+		}
+	}
+
 }
