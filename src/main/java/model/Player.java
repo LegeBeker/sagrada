9209c0cc252de2ef6a84063b5e92a5ef0e5dde04
Roy Vink <roy.vink@gmail.com>
2023-04-18 22:36:17 +0200

    We can now create and add players to the database

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -18,6 +18,24 @@ public class Player {
     private Integer idPatternCard;
 
     private int score;
+    
+    public Player createPlayer(final int gameID, final String username) {
+        Player newPlayer = new Player();
+        newPlayer.setIdGame(gameID);
+        newPlayer.setUsername(username);
+        
+        // TODO colors and refine the status
+        newPlayer.setPlayStatus("CHALLENGEE"); 
+        newPlayer.setPrivateObjCardColor("red"); 
+        
+        return newPlayer;
+    }
+    
+    public void addPlayerToDB() {
+        PlayerDB.createPlayer(this.username, this.idGame, this.playStatus, this.privateObjCardColor);
+        Map<String, String> addedPlayer = PlayerDB.getRecentPlayerFromGame(this.idGame).get(0);
+        this.setId(Integer.parseInt(addedPlayer.get("idplayer"))); // also adds the idplayer
+    }
 
     public int getId() {
         return this.idPlayer;
