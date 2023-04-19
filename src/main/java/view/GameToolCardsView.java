d056883ac154703f8e2b9994e6161f807dacf911
Volkan Welp <volkanwelp@gmail.com>
2023-04-19 23:23:13 +0200

    ✨ Add toolcard views

--- src/main/java/view/GameToolCardsView.java
+++ src/main/java/view/GameToolCardsView.java
@@ -3,8 +3,18 @@ package main.java.view;
 import javafx.scene.layout.FlowPane;
 import main.java.controller.ViewController;
 import main.java.model.Game;
+import main.java.model.ToolCard;
 
 public class GameToolCardsView extends FlowPane {
+
+    private final int gap = 10;
+
     public GameToolCardsView(final ViewController view, final Game game) {
+
+        for (ToolCard toolCard : game.getToolCards()) {
+            this.getChildren().add(new GameToolCardView(view, toolCard));
+        }
+
+        this.setHgap(gap);
     }
 }
