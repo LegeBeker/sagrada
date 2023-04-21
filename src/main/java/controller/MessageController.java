12f88497f751491abd768642b7438840775a1d35
Mike de Groot <mike251105@gmail.com>
2023-04-21 20:45:20 +0200

    🚧 ✨ sendMessage function

--- src/main/java/controller/MessageController.java
+++ src/main/java/controller/MessageController.java
@@ -1,18 +1,30 @@
 package main.java.controller;
 
+import java.sql.Timestamp;
+
 import main.java.model.Game;
+import main.java.model.GameMessage;
 
 public class MessageController {
     private String username;
     private int idGame;
     private int idPlayer;
 
+    private GameMessage message;
+
+    private Timestamp time;
+
     public void sendMessage(final String message, final ViewController view, final Game game) {
         username = view.getAccountController().getAccount().getUsername();
         idGame = game.getId();
         idPlayer = game.getPlayer(username).getId();
 
-        System.out.println(idPlayer);
+        time = new Timestamp(System.currentTimeMillis());
+
+        this.message = new GameMessage(message, idPlayer, time);
+
+        System.out.println(this.message.getMessage() + Integer.toString(this.message.getIdPlayer()) +
+                this.message.getTime().toString());
 
         // Message.sendMessage(message, game, idPlayer);
     }
