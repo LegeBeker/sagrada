17631dc6a31468fe9042a97ef85992f252d7ea69
Mike de Groot <mike251105@gmail.com>
2023-04-21 11:45:01 +0200

    🚧 begin of Message functions

--- src/main/java/model/Message.java
+++ src/main/java/model/Message.java
@@ -1,8 +1,15 @@
 package main.java.model;
 
+import java.sql.Timestamp;
+
 import main.java.db.MessageDB;
 
 public class Message {
+
+    public void sendMessage(final String message, final int idGame, final int playerId) {
+        Timestamp time = new Timestamp(System.currentTimeMillis());
+        MessageDB.addMessage(message, playerId, time);
+    }
 
     public void addMessage(final String message, final int playerId) {
 
