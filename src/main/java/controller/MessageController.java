c25ace12fc417bd208f223eaba81cf9c43935504
Mike de Groot <mike251105@gmail.com>
2023-04-21 11:45:19 +0200

    🚧 Begin of message fucntions

--- src/main/java/controller/MessageController.java
+++ src/main/java/controller/MessageController.java
@@ -1,5 +1,14 @@
 package main.java.controller;
 
+import main.java.model.Game;
+import main.java.model.Message;
+
 public class MessageController {
+    private String username;
+    private int idGame;
+
+    public void sendMessage(final String message, final ViewController view, final Game game) {
+        Message.sendMessage(message, game.getIdGame(), view.getAccount().getIdPlayer());
+    }
 
 }
