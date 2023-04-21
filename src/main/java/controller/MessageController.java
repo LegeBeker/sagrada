56056c21e97c7e7541fbe35f5632d3df4ee3ce78
Mike de Groot <mike251105@gmail.com>
2023-04-21 13:29:13 +0200

    ✨ able to get idPlayer

--- src/main/java/controller/MessageController.java
+++ src/main/java/controller/MessageController.java
@@ -1,14 +1,20 @@
 package main.java.controller;
 
 import main.java.model.Game;
-import main.java.model.Message;
 
 public class MessageController {
     private String username;
     private int idGame;
+    private int idPlayer;
 
     public void sendMessage(final String message, final ViewController view, final Game game) {
-        Message.sendMessage(message, game.getIdGame(), view.getAccount().getIdPlayer());
+        username = view.getAccountController().getAccount().getUsername();
+        idGame = game.getId();
+        idPlayer = game.getPlayer(username).getId();
+
+        System.out.println(idPlayer);
+
+        // Message.sendMessage(message, game, idPlayer);
     }
 
 }
