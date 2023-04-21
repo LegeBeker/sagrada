a833acdbc6d06fc4dc14b824a8847e86181b5aae
Mike de Groot <mike251105@gmail.com>
2023-04-21 21:29:31 +0200

    ✨ getMessages function

--- src/main/java/db/MessageDB.java
+++ src/main/java/db/MessageDB.java
@@ -1,9 +1,13 @@
 package main.java.db;
 
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import main.java.model.GameMessage;
+import main.java.model.Player;
 
 public final class MessageDB {
 
@@ -25,13 +29,25 @@ public final class MessageDB {
         return db.exec(sql, params);
     }
 
-    public static List<Map<String, String>> getMessages(final int playerId) {
+    public static List<GameMessage> getMessages(final int idGame) {
         Database db = Database.getInstance();
+        List<GameMessage> gameMessages = new ArrayList<>();
+        List<Map<String, String>> messageMaps;
 
-        String sql = "SELECT * FROM chatline WHERE idplayer = ?";
-        String[] params = {Integer.toString(playerId)};
+        String sql = "SELECT * FROM chatline INNER JOIN player ON player.idplayer = chatline.idplayer WHERE idGame = ?;";
+        String[] params = { Integer.toString(idGame) };
 
-        return db.exec(sql, params);
+        messageMaps = db.exec(sql, params);
+
+        for (Map<String, String> messageMap : messageMaps) {
+            int idPlayer = Integer.parseInt(messageMap.get("idPlayer"));
+            Timestamp time = Timestamp.valueOf(messageMap.get("time"));
+            String message = messageMap.get("message");
+
+            gameMessages.add(new GameMessage(message, Player.get(idPlayer), time));
+        }
+
+        return gameMessages;
     }
 
 }
