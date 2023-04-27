b2a7d59b4fb5389522b54f3577ee1f72ea5b12ae
Mike de Groot <mike251105@gmail.com>
2023-04-27 14:03:28 +0200

    🎨

--- src/main/java/model/GameMessage.java
+++ src/main/java/model/GameMessage.java
@@ -2,48 +2,27 @@ package main.java.model;
 
 import java.sql.Timestamp;
 
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import main.java.db.MessageDB;
-import main.java.pattern.Observable;
-
 public class GameMessage {
-    private SimpleStringProperty message;
-    private SimpleIntegerProperty idPlayer;
+    private String message;
+    private int idPlayer;
     private Timestamp time;
-    private Player player;
 
-    public GameMessage(final String message, final Player player, final Timestamp time) {
-        this.message = new SimpleStringProperty(message);
+    GameMessage(final String message, final int idPlayer, final Timestamp time) {
+        this.message = message;
+        this.idPlayer = idPlayer;
         this.time = time;
-        this.idPlayer = new SimpleIntegerProperty(player.getId());
-        this.player = player;
-
-        MessageDB.createMessage(this);
     }
 
     public String getMessage() {
-        return message.get();
-    }
-
-    public SimpleStringProperty messageProperty() {
         return message;
     }
 
     public int getIdPlayer() {
-        return idPlayer.get();
-    }
-
-    public SimpleIntegerProperty idPlayerProperty() {
         return idPlayer;
     }
 
     public Timestamp getTime() {
-        return this.time;
-    }
-
-    public Player getPlayer() {
-        return this.player;
+        return time;
     }
 
 }
