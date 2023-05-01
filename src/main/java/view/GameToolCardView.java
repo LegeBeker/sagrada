e5a9229c69ad35e0b63d58272c7929f1afa026bc
Eren <he.unal@student.avans.nl>
2023-05-01 17:14:17 +0200

    Update GameToolCardView.java

--- src/main/java/view/GameToolCardView.java
+++ src/main/java/view/GameToolCardView.java
@@ -15,21 +15,18 @@ public class GameToolCardView extends ImageView {
     private final int offset = 100;
 
     public GameToolCardView(final ViewController view, final ToolCard toolCard) {
-        this.imageToolCard = new Image("file:resources/img/toolcards/"
-                + toolCard.getName().toLowerCase().replace(" ", "-") + ".png");
+        this.imageToolCard = new Image(
+                "file:resources/img/toolcards/" + toolCard.getName().toLowerCase().replace(" ", "-") + ".png");
 
         this.setFitWidth(this.width);
         this.setFitHeight(this.height);
 
         this.setImage(this.imageToolCard);
-        
+
+        view.effects().add3DHoverEffect(this, width, height, scaleIncrease, offset);
+
         this.setOnMouseClicked(event -> {
             System.out.println(toolCard.getName() + " has been clicked!");
         });
-
-        view.effects().add3DHoverEffect(this, width, height, scaleIncrease, offset);
     }
-    
-    
-   
 }
