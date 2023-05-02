1f2a3aea16a3a80dd1014d8e6814316495af508c
Lars van Herwijnen <77294984+larsvanherwijnen@users.noreply.github.com>
2023-05-02 12:56:43 +0200

    ✨ Add function to check if board is empty

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -14,4 +14,26 @@ public class Board {
     public Die getField(final int row, final int column) {
         return board[row - 1][column - 1];
     }
+
+    public Boolean isEmpty() {
+        boolean containsItems = false;
+
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLUMNS; j++) {
+                if (board[i][j] != null) {
+                    containsItems = true;
+                    break;
+                }
+            }
+            if (containsItems) {
+                break;
+            }
+        }
+
+        if (containsItems) {
+            return false;
+        }
+
+        return true;
+    }
 }
