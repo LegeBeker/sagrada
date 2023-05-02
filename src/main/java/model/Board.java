f8f22af655d18a2135f0f148c6abd82c95daa684
Lars van Herwijnen <77294984+larsvanherwijnen@users.noreply.github.com>
2023-05-02 08:55:31 +0200

    ✨ Created board class

---
+++ src/main/java/model/Board.java
@@ -0,0 +1,14 @@
+package main.java.model;
+
+public class Board {
+
+    private static final int ROWS = 4;
+    private static final int COLUMNS = 5;
+
+    private Die[][] board = new Die[ROWS][COLUMNS];
+
+    public Board() {
+
+    }
+
+}
--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -17,11 +17,6 @@ public class Player {
     private String privateObjCardColor;
     private Integer idPatternCard;
 
-    private static final int ROWS = 4;
-    private static final int COLUMNS = 5;
-
-    private Die[][] board = new Die[ROWS][COLUMNS];
-
     private int score;
 
     public Player createPlayer(final int gameID, final String username) {
