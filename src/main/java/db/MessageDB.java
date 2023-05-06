5a07bbf6b0273fe136c477480d41dac17b6bbc3c
Lars van Herwijnen <77294984+larsvanherwijnen@users.noreply.github.com>
2023-05-06 20:30:21 +0200

    ✨ Update message queries

--- src/main/java/db/MessageDB.java
+++ src/main/java/db/MessageDB.java
@@ -5,34 +5,33 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import main.java.model.ChatLine;
-import main.java.model.Message;
 import main.java.model.Player;
 
 public final class MessageDB {
 
     private MessageDB() {
-
     }
-
     // Create methods to add messages to the database
-    public static List<Map<String, String>> createMessage(final String message, final Player player,
+    public static boolean createMessage(final String message, final Player player,
             final Timestamp time) {
         Database db = Database.getInstance();
 
         String sql = "INSERT INTO chatline (idPlayer, time, message) VALUES (?, ?, ?);";
         String[] params = {Integer.toString(player.getId()), time.toString(), message};
 
-        return db.exec(sql, params);
+        db.exec(sql, params);
+
+        return true;
+
     }
 
-    public static List<Map<String, String>> getMessages(final int idGame) {
+    public static List<Map<String, String>> getChatMessages(final int gameId) {
         Database db = Database.getInstance();
 
-        String sql = "SELECT * FROM chatline INNER JOIN player ON player.idplayer = chatline.idplayer WHERE idGame = ?;";
-        String[] params = {Integer.toString(idGame)};
+        String sql = "SELECT * FROM chatline INNER JOIN player ON player.idplayer = chatline.idplayer WHERE player.idgame = ? ORDER BY time ASC;";
+        String[] params = {Integer.toString(gameId)};
 
         return db.exec(sql, params);
     }
-
+ 
 }
