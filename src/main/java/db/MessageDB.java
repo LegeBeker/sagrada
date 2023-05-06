fec99667bccfa17db2e3f78cb47ac1fecf625fba
Lars van Herwijnen <77294984+larsvanherwijnen@users.noreply.github.com>
2023-05-06 19:13:50 +0200

    ♻️ Updated queries

--- src/main/java/db/MessageDB.java
+++ src/main/java/db/MessageDB.java
@@ -1,12 +1,12 @@
 package main.java.db;
 
-import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import main.java.model.GameMessage;
+import main.java.model.ChatLine;
+import main.java.model.Message;
 import main.java.model.Player;
 
 public final class MessageDB {
@@ -16,38 +16,23 @@ public final class MessageDB {
     }
 
     // Create methods to add messages to the database
-    public static List<Map<String, String>> createMessage(final GameMessage gameMessage) {
+    public static List<Map<String, String>> createMessage(final String message, final Player player,
+            final Timestamp time) {
         Database db = Database.getInstance();
 
         String sql = "INSERT INTO chatline (idPlayer, time, message) VALUES (?, ?, ?);";
-        String[] params = {
-                String.valueOf(gameMessage.getIdPlayer()),
-                String.valueOf(gameMessage.getTime()),
-                gameMessage.getMessage()
-        };
+        String[] params = {Integer.toString(player.getId()), time.toString(), message};
 
         return db.exec(sql, params);
     }
 
-    public static List<GameMessage> getMessages(final int idGame) {
+    public static List<Map<String, String>> getMessages(final int idGame) {
         Database db = Database.getInstance();
-        List<GameMessage> gameMessages = new ArrayList<>();
-        List<Map<String, String>> messageMaps;
 
         String sql = "SELECT * FROM chatline INNER JOIN player ON player.idplayer = chatline.idplayer WHERE idGame = ?;";
-        String[] params = { Integer.toString(idGame) };
+        String[] params = {Integer.toString(idGame)};
 
-        messageMaps = db.exec(sql, params);
-
-        for (Map<String, String> messageMap : messageMaps) {
-            int idPlayer = Integer.parseInt(messageMap.get("idPlayer"));
-            Timestamp time = Timestamp.valueOf(messageMap.get("time"));
-            String message = messageMap.get("message");
-
-            gameMessages.add(new GameMessage(message, Player.get(idPlayer), time));
-        }
-
-        return gameMessages;
+        return db.exec(sql, params);
     }
 
 }
