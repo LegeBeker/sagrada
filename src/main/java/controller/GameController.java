67f2de22f3e4002043adc61360dabb332b6e0b8a
Mike de Groot <mike251105@gmail.com>
2023-05-11 20:32:49 +0200

    ✨ get function for color

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -26,13 +26,19 @@ public final class GameController {
         return this.game;
     }
 
-    public Game createGame(final ArrayList<Account> accounts, final Account currAccount, final boolean useDefaultCards) {
+    public Game createGame(final ArrayList<Account> accounts, final Account currAccount,
+            final boolean useDefaultCards) {
         return Game.createGame(accounts, currAccount, useDefaultCards);
     }
 
     public Player getCurrentPlayer(final int idGame) {
         Game game = Game.get(idGame);
         return game.getCurrentPlayer(idGame, this.view.getAccountController().getAccount().getUsername());
+    }
+
+    public String getPrivateObjectiveColor(final int idPlayer) {
+        Player player = Player.get(idPlayer);
+        return player.getPrivateObjective().getColor();
     }
 
     public void choosePatternCard(final PatternCard patternCard) {
