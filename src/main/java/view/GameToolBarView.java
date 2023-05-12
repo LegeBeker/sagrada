3568f7c00f523afc09964bdd187c35fc82429887
Mike de Groot <mike251105@gmail.com>
2023-05-12 11:39:10 +0200

    ✨ parsed right arguments

--- src/main/java/view/GameToolBarView.java
+++ src/main/java/view/GameToolBarView.java
@@ -9,7 +9,7 @@ public class GameToolBarView extends HBox {
 
     public GameToolBarView(final ViewController view, final Game game) {
         this.getChildren().addAll(new GameToolCardsView(view, game),
-                new GameGoalCardsView(view),
+                new GameGoalCardsView(view, game),
                 new RoundTrackView(view, game));
 
         this.getChildren().forEach(child -> {
