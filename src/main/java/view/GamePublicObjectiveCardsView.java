ce4ff52d5ec51c7a6666d310daf48c745b60f786
Roy Vink <roy.vink@gmail.com>
2023-05-16 15:42:18 +0200

    :sparkles: Added a VGap

--- src/main/java/view/GamePublicObjectiveCardsView.java
+++ src/main/java/view/GamePublicObjectiveCardsView.java
@@ -7,13 +7,15 @@ import main.java.model.ObjectiveCard;
 
 public class GamePublicObjectiveCardsView extends FlowPane {
 
-    private static final int GAP = 25;
+    private static final int HGAP = 25;
+    private static final int VGAP = 10;
 
     public GamePublicObjectiveCardsView(final ViewController view, final Game game) {
         for (ObjectiveCard publicObjectiveCard : game.getPublicToolcards()) {
             this.getChildren().add(new GamePublicObjectiveCardView(view, publicObjectiveCard));
         }
 
-        this.setHgap(GAP);
+        this.setHgap(HGAP);
+        this.setVgap(VGAP);
     }
 }
