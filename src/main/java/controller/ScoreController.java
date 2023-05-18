06e75173ddccb2ec5bd34cf92e2d79b4021cd9ea
Mike de Groot <mike251105@gmail.com>
2023-05-18 21:25:10 +0200

    🚧 Score functions

--- src/main/java/controller/ScoreController.java
+++ src/main/java/controller/ScoreController.java
@@ -4,11 +4,11 @@ import main.java.model.Game;
 import main.java.model.Player;
 
 public class ScoreController {
-    private int score = -20;
     private int playerId;
 
     public void calculateScore(final Player player, final Game game, final String privateObjCardColor,
             final int favorTokens) {
+        int score = -20;
         System.out.println("Calculating score for player " + player.getId() + " in game " + game.getId()
                 + " with private objective card "
                 + privateObjCardColor + " and " + favorTokens + " favor tokens.");
@@ -16,12 +16,27 @@ public class ScoreController {
         score += favorTokens;
         System.out.println("current score: " + score);
         score += getAmountOfDice(player);
+        System.out.println("Adding: " + getAmountOfDice(player));
+        System.out.println("current score: " + score);
+        System.out.println("Adding: " + getPrivateObjectiveScore(player));
+        score += getPrivateObjectiveScore(player);
         System.out.println("current score: " + score);
     }
 
     private int getAmountOfDice(final Player player) {
         System.out.println("Amount of dice: " + player.getBoard().getAmountOfDice());
         return player.getBoard().getAmountOfDice();
+    }
+
+    private int getPrivateObjectiveScore(final Player player) {
+        System.out.println("Private objective score: "
+                + player.getBoard().getPrivateObjectiveCardScore(player.getPrivateObjCardColor().toUpperCase()));
+        return player.getBoard().getPrivateObjectiveCardScore(player.getPrivateObjCardColor().toUpperCase());
+    }
+
+    private int getPublicObjectiveScore(final Player player) {
+        System.out.println("Public objective score: " + player.getBoard().getPublicObjectiveCardScore());
+        return player.getBoard().getPublicObjectiveCardScore(player.getGame().getObjectiveCardsIds());
     }
 
 }
