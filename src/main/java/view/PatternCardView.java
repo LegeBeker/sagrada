bc2e9d32067377bbeb1bb839ed04e475bd811279
Lars van Herwijnen <77294984+larsvanherwijnen@users.noreply.github.com>
2023-05-19 20:39:52 +0200

    ♻️ Refactor code

--- src/main/java/view/PatternCardView.java
+++ src/main/java/view/PatternCardView.java
@@ -126,7 +126,7 @@ public class PatternCardView extends BorderPane implements Observer {
         ArrayList<int[]> locations = new ArrayList<int[]>();
         grid.getChildren().forEach((e) -> {
             if (e.getStyle().contains("-fx-border-color: #00FFBF;")) {
-                int[] location = { GridPane.getRowIndex(e), GridPane.getColumnIndex(e) };
+                int[] location = {GridPane.getRowIndex(e), GridPane.getColumnIndex(e)};
                 locations.add(location);
             }
         });
@@ -137,7 +137,7 @@ public class PatternCardView extends BorderPane implements Observer {
 
         grid.getChildren().forEach((cell) -> {
             locations.forEach((cellLocation) -> {
-                int[] location = { GridPane.getRowIndex(cell), GridPane.getColumnIndex(cell) };
+                int[] location = {GridPane.getRowIndex(cell), GridPane.getColumnIndex(cell)};
                 if (cellLocation[0] == location[0] && cellLocation[1] == location[1]) {
                     cell.setStyle("-fx-border-color: #00FFBF;");
                 }
