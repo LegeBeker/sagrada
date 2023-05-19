07c8aeca6a81759675c6a8179e24da27628a5adf
Lars van Herwijnen <77294984+larsvanherwijnen@users.noreply.github.com>
2023-05-19 20:36:24 +0200

    🐛 Fix bug in border removed after update

--- src/main/java/view/PatternCardView.java
+++ src/main/java/view/PatternCardView.java
@@ -1,5 +1,6 @@
 package main.java.view;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 import javafx.geometry.Insets;
@@ -122,8 +123,26 @@ public class PatternCardView extends BorderPane implements Observer {
 
     @Override
     public void update() {
+        ArrayList<int[]> locations = new ArrayList<int[]>();
+        grid.getChildren().forEach((e) -> {
+            if (e.getStyle().contains("-fx-border-color: #00FFBF;")) {
+                int[] location = { GridPane.getRowIndex(e), GridPane.getColumnIndex(e) };
+                locations.add(location);
+            }
+        });
+
         grid.getChildren().clear();
+
         drawPatternCard(view, patternCardId, playerId);
+
+        grid.getChildren().forEach((cell) -> {
+            locations.forEach((cellLocation) -> {
+                int[] location = { GridPane.getRowIndex(cell), GridPane.getColumnIndex(cell) };
+                if (cellLocation[0] == location[0] && cellLocation[1] == location[1]) {
+                    cell.setStyle("-fx-border-color: #00FFBF;");
+                }
+            });
+        });
     }
 
     private void drawPatternCard(final ViewController view, final int patternCardId, final Integer playerId) {
