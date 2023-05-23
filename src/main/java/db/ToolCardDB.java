b202632e7077f92be31f3b199a78e1686d679564
Eren <he.unal@student.avans.nl>
2023-05-23 23:28:00 +0200

    Update ToolCardDB.java

--- src/main/java/db/ToolCardDB.java
+++ src/main/java/db/ToolCardDB.java
@@ -9,10 +9,8 @@ public final class ToolCardDB {
 
     public static List<Map<String, String>> getToolCards(final int idGame) {
         Database db = Database.getInstance();
-
         String sql = "SELECT * FROM toolcard tc JOIN gametoolcard gtc ON tc.idtoolcard = gtc.idtoolcard WHERE gtc.idgame = ?";
         String[] params = { Integer.toString(idGame) };
-
         return db.exec(sql, params);
     }
 
