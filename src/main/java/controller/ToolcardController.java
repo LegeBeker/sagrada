d727be8f9b15ecf3d74e1b4b2e024b439b066869
Eren <he.unal@student.avans.nl>
2023-05-23 23:16:28 +0200

    Update ToolcardController.java

--- src/main/java/controller/ToolcardController.java
+++ src/main/java/controller/ToolcardController.java
@@ -83,7 +83,8 @@ public class ToolcardController {
                 }
                 ToolCardDB.updateGameDieValue(dieValue, 0);
                 System.out.println("Die flipped. New value: " + dieValue);
-            } else { System.out.println("Invalid command.");
+            } else {
+                System.out.println("Invalid command.");
             }
         }
     }
