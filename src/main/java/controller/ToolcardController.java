91c2441460ff83bb04179c9435b268676e59bed1
Eren <he.unal@student.avans.nl>
2023-05-26 17:52:59 +0200

    Update ToolcardController.java

--- src/main/java/controller/ToolcardController.java
+++ src/main/java/controller/ToolcardController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
-
 import javafx.scene.paint.Color;
 import main.java.db.ToolCardDB;
 import main.java.model.Die;
