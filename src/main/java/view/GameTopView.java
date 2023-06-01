14f5e3a104b8d55353c5fddc61f9afe490b17c81
Roy Vink <roy.vink@gmail.com>
2023-06-01 14:26:50 +0200

    ✨ The invisible rectangle is now based on screen width

--- src/main/java/view/GameTopView.java
+++ src/main/java/view/GameTopView.java
@@ -3,15 +3,17 @@ package main.java.view;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Screen;
 import main.java.controller.ViewController;
 
 public class GameTopView extends HBox{
 
-    private static final int COUNTERTOPVIEWWIDTH = 450;
+    private final static double PARTOFTHESCREEN = 4.75;
     
     public GameTopView(final ViewController view) {
         super();
-        this.getChildren().addAll(new GameOfferView(view), new RoundTrackView(view), new Rectangle(COUNTERTOPVIEWWIDTH, 0));
+        final double screenWidth = Screen.getPrimary().getBounds().getMaxX();
+        this.getChildren().addAll(new GameOfferView(view), new RoundTrackView(view), new Rectangle(screenWidth / PARTOFTHESCREEN, 0));
         this.getChildren().forEach(child -> {
             HBox.setHgrow(child, Priority.ALWAYS);
         });
