ab062356f8ae0cc0277a0e00848c21d069fd943e
Volkan Welp <volkanwelp@gmail.com>
2023-06-02 11:52:05 +0200

    ♻️ Refactor code

--- src/main/java/db/GameDB.java
+++ src/main/java/db/GameDB.java
@@ -24,15 +24,13 @@ public final class GameDB {
         Database db = Database.getInstance();
 
         String sql = "SELECT game.*, DATE_FORMAT(game.creationdate, '%d-%m-%Y %H:%i:%s') AS formatted_creationdate, player.username, round.roundnr, "
-                +
-                "CASE WHEN pl2.username IS NOT NULL THEN 'TRUE' ELSE 'FALSE' END AS isPlayerInGame, " +
-                "CASE WHEN EXISTS (SELECT 1 FROM player pl3 WHERE game.idgame = pl3.idgame AND pl3.playstatus = 'refused') THEN 'TRUE' ELSE 'FALSE' END AS hasDeclinedInvites "
-                +
-                "FROM game " +
-                "LEFT JOIN player ON game.turn_idplayer = player.idplayer " +
-                "LEFT JOIN player pl2 ON game.idgame = pl2.idgame AND pl2.username = ? " +
-                "LEFT JOIN round ON round.roundID = game.current_roundID " +
-                "GROUP BY game.idgame;";
+                + "CASE WHEN pl2.username IS NOT NULL THEN 'TRUE' ELSE 'FALSE' END AS isPlayerInGame, "
+                + "CASE WHEN EXISTS (SELECT 1 FROM player pl3 WHERE game.idgame = pl3.idgame AND pl3.playstatus = 'refused') THEN 'TRUE' ELSE 'FALSE' END AS hasDeclinedInvites "
+                + "FROM game "
+                + "LEFT JOIN player ON game.turn_idplayer = player.idplayer "
+                + "LEFT JOIN player pl2 ON game.idgame = pl2.idgame AND pl2.username = ? "
+                + "LEFT JOIN round ON round.roundID = game.current_roundID "
+                + "GROUP BY game.idgame;";
 
         String[] params = {username};
 
