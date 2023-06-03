cf6badbee431e1fda5bb824dd249e2dd0515043c
Volkan Welp <volkanwelp@gmail.com>
2023-06-03 22:09:32 +0200

    💄 Fix toolcard name

--- src/main/java/enums/ToolcardEnum.java
+++ src/main/java/enums/ToolcardEnum.java
@@ -1,38 +1,126 @@
 package main.java.enums;
 
 public enum ToolcardEnum {
-    GROZINGPLIERS("Grozing Pliers", "Grozingtang", "grozingPliers"),
-    COPPERFOILBURNISHER("Copper Foil Burnisher", "Koperfoliebrander", "copperFoilBurnisher"),
-    LATHEKIN("Lathekin", "Lathekin", "lathekin"),
-    LENSCUTTER("Lens Cutter", "Lenssnijder", "lensCutter"),
-    FLUXBRUSH("Flux Brush", "Fluxkwast", "fluxBrush"),
-    GLAZINGHAMMER("Glazing Hammer", "Glazuurhamer", "blazingHammer"),
-    RUNNINGPLIERS("Running Pliers", "Snijtang", "runningPliers"),
-    CORKBACKEDSTRAIGHTEDGE("Cork-backed Straightedge", "Kurkliniaal", "corkBackedStraightedge"),
-    GRINDINGSTONE("Grinding Stone", "Slijpsteen", "grindingStone"),
-    FLUXREMOVER("Flux Remover", "Fluxremover", "fluxRemover"),
-    TAPWHEEL("Tap Wheel", "Tapwiel", "tapWheel"),
-    EGLOMISEBRUSH("Eglomise Brush", "Eglomisekwast", "eglomiseBrush");
+    GROZINGPLIERS("Grozing Pliers"),
+    COPPERFOILBURNISHER("Copper Foil Burnisher"),
+    LATHEKIN("Lathekin"),
+    LENSCUTTER("Lens Cutter"),
+    FLUXBRUSH("Flux Brush"),
+    GLAZINGHAMMER("Glazing Hammer"),
+    RUNNINGPLIERS("Running Pliers"),
+    CORKBACKEDSTRAIGHTEDGE("Cork-backed Straightedge"),
+    GRINDINGSTONE("Grinding Stone"),
+    FLUXREMOVER("Flux Remover"),
+    TAPWHEEL("Tap Wheel"),
+    EGLOMISEBRUSH("Eglomise Brush");
 
-    private final String englishName;
-    private final String dutchName;
-    private final String methodName;
+    private final String name;
 
-    ToolcardEnum(final String englishName, final String dutchName, final String methodName) {
-        this.englishName = englishName;
-        this.dutchName = dutchName;
-        this.methodName = methodName;
+    ToolcardEnum(final String name) {
+        this.name = name;
     }
 
-    public String getEnglishName() {
-        return englishName;
+    public String getName() {
+        return name;
+    }
+
+    public static ToolcardEnum fromString(final String name) {
+        if (name == null) {
+            return null;
+        }
+        switch (name.toLowerCase()) {
+            case "grozing pliers":
+                return GROZINGPLIERS;
+            case "copper foil burnisher":
+                return COPPERFOILBURNISHER;
+            case "lathekin":
+                return LATHEKIN;
+            case "lens cutter":
+                return LENSCUTTER;
+            case "flux brush":
+                return FLUXBRUSH;
+            case "glazing hammer":
+                return GLAZINGHAMMER;
+            case "running pliers":
+                return RUNNINGPLIERS;
+            case "cork-backed straightedge":
+                return CORKBACKEDSTRAIGHTEDGE;
+            case "grinding stone":
+                return GRINDINGSTONE;
+            case "flux remover":
+                return FLUXREMOVER;
+            case "tap wheel":
+                return TAPWHEEL;
+            case "eglomise brush":
+                return EGLOMISEBRUSH;
+            default:
+                return null;
+        }
     }
 
     public String getDutchName() {
-        return dutchName;
+        switch (this) {
+            case GROZINGPLIERS:
+                return "Tang";
+            case COPPERFOILBURNISHER:
+                return "Brander";
+            case LATHEKIN:
+                return "Puntenslijper";
+            case LENSCUTTER:
+                return "Snijder";
+            case FLUXBRUSH:
+                return "Kwast";
+            case GLAZINGHAMMER:
+                return "Hammer";
+            case RUNNINGPLIERS:
+                return "Tang";
+            case CORKBACKEDSTRAIGHTEDGE:
+                return "Liniaal";
+            case GRINDINGSTONE:
+                return "Slijpsteen";
+            case FLUXREMOVER:
+                return "Remover";
+            case TAPWHEEL:
+                return "Wiel";
+            case EGLOMISEBRUSH:
+                return "Kwast";
+            default:
+                return null;
+        }
     }
 
     public String getMethodName() {
-        return methodName;
+        switch (this) {
+            case GROZINGPLIERS:
+                return "grozingPliers";
+            case COPPERFOILBURNISHER:
+                return "copperFoilBurnisher";
+            case LATHEKIN:
+                return "lathekin";
+            case LENSCUTTER:
+                return "lensCutter";
+            case FLUXBRUSH:
+                return "fluxBrush";
+            case GLAZINGHAMMER:
+                return "glazingHammer";
+            case RUNNINGPLIERS:
+                return "runningPliers";
+            case CORKBACKEDSTRAIGHTEDGE:
+                return "corkBackedStraightedge";
+            case GRINDINGSTONE:
+                return "grindingStone";
+            case FLUXREMOVER:
+                return "fluxRemover";
+            case TAPWHEEL:
+                return "tapWheel";
+            case EGLOMISEBRUSH:
+                return "eglomiseBrush";
+            default:
+                return null;
+        }
+    }
+
+    public String getImageName() {
+        return name.toLowerCase().replace(" ", "-");
     }
 }
